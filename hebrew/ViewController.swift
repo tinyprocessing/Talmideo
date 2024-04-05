81a6103a714ae76962b5f0e36ca4dd8e5bb35c9e
Mikhail Safir <safir@thenoco.co>
2024-04-05 12:28:02 +0300

    Initial Commit

---
+++ hebrew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hebrew
+//
+//  Created by Mikhail Safir on 05/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
